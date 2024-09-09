e2aa8323fb689771d13c532db95015b209f1cbe2
Djibril-6et <djibril.6et@gmail.com>
2024-09-09 23:06:49 +0200

    add login inputs

--- src/pages/connexion/index.jsx
+++ src/pages/connexion/index.jsx
@@ -1,18 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './index.css';  // Importer le fichier CSS
 
 function Connexion({ setIsAuthenticated }) {
   const navigate = useNavigate();
+  
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
 
   const handleLogin = () => {
-    setIsAuthenticated(true);  // Met à jour l'état d'authentification
-    navigate('/');  // Redirige vers la page d'accueil
+    console.log(user);
   };
 
   return (
     <div className="connexion-container">
       <h2 className="connexion-title">Connexion</h2>
+      
+      {/* Champ pour l'email */}
+      <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        value={user.email}
+        onChange={handleChange}
+        className="connexion-input"
+      />
+      
+      {/* Champ pour le mot de passe */}
+      <input
+        type="password"
+        name="password"
+        placeholder="Mot de passe"
+        value={user.password}
+        onChange={handleChange}
+        className="connexion-input"
+      />
+      
+      {/* Bouton pour soumettre le formulaire de connexion */}
       <button className="connexion-button" onClick={handleLogin}>
         Me connecter
       </button>
